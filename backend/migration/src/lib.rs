c807970a5ee5b71bbf586e76fdacef37f0d47865
Tiamat-KIT <fill.ayaextech@gmail.com>
2025-05-30 08:54:29 +0000

    refactor: 必要のないmigrationを削除する

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -9,7 +9,6 @@ mod m20250530_000003_consolidated_outfits;
 mod m20250530_000004_consolidated_join_tables;
 mod m20250530_000005_consolidated_categories_and_tags;
 mod m20250530_000006_consolidated_user_preferences;
-mod m20250530_000007_consolidated_wear_histories;
 
 pub struct Migrator;
 
@@ -21,12 +20,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250530_000002_consolidated_clothings::Migration),
             Box::new(m20250530_000003_consolidated_outfits::Migration),
             Box::new(m20250530_000004_consolidated_join_tables::Migration),
-            // The following migrations reference tables that might not exist yet
-            // Uncomment them when those tables are created
-            // Box::new(m20250530_000005_consolidated_categories_and_tags::Migration),
             Box::new(m20250530_000006_consolidated_user_preferences::Migration),
-            // Box::new(m20250530_000007_consolidated_wear_histories::Migration),
-
             // inject-above (do not remove this comment)
         ]
     }
